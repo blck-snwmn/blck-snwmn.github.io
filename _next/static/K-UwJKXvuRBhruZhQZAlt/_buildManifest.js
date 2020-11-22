71b0fdff37fd1a8513777b3c52acf2763fa16da8
blck-snwmn <blck-snwmn@users.noreply.github.com>
2020-11-22 14:59:38 +0000

    deploy: ac8cb109dffe00ecc492671abb484addaca16399

---
+++ _next/static/K-UwJKXvuRBhruZhQZAlt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-3b98bcb8a1ca214e5361.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b1819aa8f092fceae3d2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
